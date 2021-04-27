de28a0295f2e3a86c108e48e3c5d3cab0069d61b
Agustin Fernández <agustinfernandez3297@gmail.com>
2021-04-27 16:51:51 -0300

    mi demo

---
+++ fonte/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
